1a19faa507fb91ac29b91c52a10f54a7fa5fa6b1
Lubor Kemza <kemza@notia.cz>
2017-07-10 10:58:40 +0200

    oprava názvu aplikace

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -26,7 +26,7 @@ const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 const HMR = helpers.hasProcessFlag('hot');
 const AOT = helpers.hasNpmFlag('aot');
 const METADATA = {
-  title: 'CAA AMED',
+  title: 'NBS expedition',
   baseUrl: '/',
   isDevServer: helpers.isWebpackDevServer()
 };
